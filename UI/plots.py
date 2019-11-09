9e047a7f8b2f52dbe11d13325dc56e55707154df
Err0neus <43116506+Err0neus@users.noreply.github.com>
2019-11-09 16:03:07 +0000

    added a bar version of the chart - to choose better style

--- UI/plots.py
+++ UI/plots.py
@@ -43,7 +43,6 @@ def album_song_count_per_period(discog, bin_size):
 def plot_albums_songs_per_period(discog, bin_size):
     '''plots the number of albums and songs per period'''
     
-    
     data = album_song_count_per_period(discog, bin_size)
     
     fig, ax1 = plt.subplots(figsize=(8,4))
@@ -54,7 +53,7 @@ def plot_albums_songs_per_period(discog, bin_size):
     ax1.plot(data.period, data.album, color=color)
     ax1.tick_params(axis='y', labelcolor=color)
     ax1.tick_params(axis='x', labelrotation=45)
-
+    
     ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
 
     color = 'tab:blue'
@@ -66,6 +65,37 @@ def plot_albums_songs_per_period(discog, bin_size):
     
     ax1.yaxis.set_major_locator(MaxNLocator(integer=True)) 
     ax2.yaxis.set_major_locator(MaxNLocator(integer=True)) 
+    plt.show()
+
+def plot_albums_songs_per_period_bar(discog, bin_size):
+    '''plots the number of albums and songs per period'''
+    
+    width = 0.2
+    data = album_song_count_per_period(discog, bin_size).set_index('period')
+    
+    fig, ax1 = plt.subplots(figsize=(8,4))
+
+    color = 'tab:red'
+
+    ax1.set_ylabel('number of albums', color=color)
+    #ax1.plot(data.period, data.album, color=color)
+    data.album.plot(kind='bar', color='red', ax=ax1, width=width, position=1)
+    ax1.tick_params(axis='y', labelcolor=color)
+    ax1.tick_params(axis='x', labelrotation=45)
+    ax1.set_xlabel(str(bin_size) + '-year period')
+    
+    ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+    color = 'tab:blue'
+    ax2.set_ylabel('number of songs', color=color)  # we already handled the x-label with ax1
+    #ax2.plot(data.index.tolist(), data.track_title, color=color)
+    ax2.tick_params(axis='y', labelcolor=color)
+
+    fig.tight_layout()  # otherwise the right y-label may be slightly clipped
+    data.track_title.plot(kind='bar', color='blue', ax=ax2, width=width, position=0)
+    ax1.yaxis.set_major_locator(MaxNLocator(integer=True)) 
+    ax2.yaxis.set_major_locator(MaxNLocator(integer=True)) 
+
     plt.show()
     
 # set bin_size var with default 10
@@ -86,6 +116,7 @@ def button_1_func(x):
     plots()
     #display chart using the new bin_size
     plot_albums_songs_per_period(discog, bin_size)
+    plot_albums_songs_per_period_bar(discog, bin_size)
 
     
 # UI
