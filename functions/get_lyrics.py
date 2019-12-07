9ed1888a42b73c9ecb8ee5b3fbf73799e550c766
Chitra Pun <chitra.pun@nielsen.com>
2019-12-07 20:58:39 +0000

    Added progress bar into get_lyrics function

--- functions/get_lyrics.py
+++ functions/get_lyrics.py
@@ -1,4 +1,6 @@
 import lyricsgenius
+from tqdm import tnrange, tqdm_notebook
+from tqdm.notebook import trange, tqdm
 # authenticate to GENIUS.COM
 genius = lyricsgenius.Genius("yUrV_dD4VMSNbqnx4GERtNJAdeX1121T1OqR8nZMtbQI7Tsi1vrc2Hpxmgrb0l-S")
 genius.verbose = False
@@ -9,7 +11,8 @@ def getLyrics(df):
     filter_data = df[(df["EXCLUDE_ALBUM"] == False) & (df["EXCLUDE_SONG"] == False)].copy(deep=True) 
     atrist_name = filter_data["ARTIST_NAME"].unique()[0]
     
-    for track in filter_data.TRACK_TITLE:
+    for i in tqdm_notebook(range(len(filter_data.TRACK_TITLE))):
+        track = filter_data["TRACK_TITLE"][i]
         song = genius.search_song(track, atrist_name)       
         if song is None: # to abort error when lyrics is not found for particular songs
             filter_data.loc[df["TRACK_TITLE"] == track, "LYRICS"] = "Not Found"
