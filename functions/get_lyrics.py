a1758867361bb765260c20db0c1cb863ce48a698
Chitra Pun <chitra.pun@nielsen.com>
2019-12-07 21:11:24 +0000

    changing tqdm_notebook to tqdm, as for version 5.o tqdm_notebook will not work

--- functions/get_lyrics.py
+++ functions/get_lyrics.py
@@ -1,5 +1,4 @@
 import lyricsgenius
-from tqdm import tnrange, tqdm_notebook
 from tqdm.notebook import trange, tqdm
 # authenticate to GENIUS.COM
 genius = lyricsgenius.Genius("yUrV_dD4VMSNbqnx4GERtNJAdeX1121T1OqR8nZMtbQI7Tsi1vrc2Hpxmgrb0l-S")
@@ -11,7 +10,7 @@ def getLyrics(df):
     filter_data = df[(df["EXCLUDE_ALBUM"] == False) & (df["EXCLUDE_SONG"] == False)].copy(deep=True) 
     atrist_name = filter_data["ARTIST_NAME"].unique()[0]
     
-    for i in tqdm_notebook(range(len(filter_data.TRACK_TITLE))):
+    for i in tqdm(range(len(filter_data.TRACK_TITLE))):
         track = filter_data["TRACK_TITLE"][i]
         song = genius.search_song(track, atrist_name)       
         if song is None: # to abort error when lyrics is not found for particular songs
