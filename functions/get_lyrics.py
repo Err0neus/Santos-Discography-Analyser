c8724e2204a5a1ce54d23f12684f6415f2765e73
Chitra Pun <chitra.pun@nielsen.com>
2020-01-10 15:38:23 +0000

    Added new function getLyrics_V2 # pip install lyricsmaster before runing the code

--- functions/get_lyrics.py
+++ functions/get_lyrics.py
@@ -1,6 +1,24 @@
 import lyricsgenius
 
 from tqdm import tnrange, tqdm_notebook, notebook
+
+from lyricsmaster import LyricWiki, TorController
+
+# pip install lyricsmaster
+'''
+LyricsMaster is a library for downloading lyrics from multiple lyrics providers.
+The following Lyrics Providers are supported:
+Lyric Wikia
+AzLyrics
+Genius
+Lyrics007
+MusixMatch
+The Original Hip-Hop (Rap) Lyrics Archive - OHHLA.com
+and more to come soon.
+Free software: MIT license
+Documentation: https://lyricsmaster.readthedocs.io.
+'''
+
 #from tqdm.notebook import trange, tqdm
 
 # authenticate to GENIUS.COM
@@ -29,7 +47,7 @@ def getLyrics_V2(df):
     filter_data = filter_data.reset_index(drop=True)
     atrist_name = filter_data["ARTIST_NAME"].unique()[0]
   
-    for i in tqdm_notebook(range(len(filter_data.TRACK_TITLE))):
+    for i in notebook.tqdm(range(len(filter_data.TRACK_TITLE))):
         albums = filter_data["ALBUMS"][i]
         track = filter_data["TRACK_TITLE"][i]
         fetch_lyric = provider.get_lyrics(atrist_name, album = albums, song = track)
