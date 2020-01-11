063e00e631951bc85da2aa35b8ab776a4359c945
Chitra Pun <chitra.pun@nielsen.com>
2020-01-11 15:48:12 +0000

    Add more function- Now getLyrics_V2 uses both lyricswiki and lyricsgenius..

--- functions/get_lyrics.py
+++ functions/get_lyrics.py
@@ -27,6 +27,9 @@ genius.verbose = False
 genius.remove_section_headers = True
 ##
 
+# from lyrics Master provider
+provider = LyricWiki()
+
 def getLyrics(df):
     filter_data = df[(df["EXCLUDE_ALBUM"] == False) & (df["EXCLUDE_SONG"] == False)].copy(deep=True) 
     atrist_name = filter_data["ARTIST_NAME"].unique()[0]
@@ -42,7 +45,37 @@ def getLyrics(df):
             
     return filter_data
 
+
+def getLyricsGenius(df, df2, atrist_name):
+    '''
+    param: df-> filter dataframe
+    param: df2-> master dataframe
+    param: atrist_name-> name of particular atrist "String"
+    
+    return: datafarme with "LYRICS" column
+    '''
+    print("Fetching lyrics from LyricsGenius...")
+    
+    for i in notebook.tqdm(range(len(df.TRACK_TITLE))):
+        track = df.reset_index(drop=True)["TRACK_TITLE"][i]
+        song = genius.search_song(track, atrist_name)       
+        if song is None: # to abort error when lyrics is not found for particular songs
+            df2.loc[df2["TRACK_TITLE"] == track, "LYRICS"] = "Not Found"
+        else:
+            df2.loc[df2["TRACK_TITLE"] == track, "LYRICS"] = song.lyrics
+            
+    return df2
+
+
 def getLyrics_V2(df):
+    '''
+    Fetching lyric for each song using lyricsmaster
+    If there are any song without any lyrics then fetch lyrics from lyricsgenius
+    
+    param: df-> dataframe
+    
+    return: dataframe with LYRICS column
+    '''
     filter_data = df[(df["EXCLUDE_ALBUM"] == False) & (df["EXCLUDE_SONG"] == False)].copy(deep=True)
     filter_data = filter_data.reset_index(drop=True)
     atrist_name = filter_data["ARTIST_NAME"].unique()[0]
@@ -54,5 +87,11 @@ def getLyrics_V2(df):
         for album in fetch_lyric:
             for i_song in album:
                 filter_data.loc[filter_data["TRACK_TITLE"] == track, "LYRICS"] = i_song.lyrics
+
+    if filter_data["LYRICS"].isnull:
+        data = filter_data[filter_data["LYRICS"].isnull()]
+        final_data = getLyricsGenius(data, filter_data, atrist_name)
+        return final_data
     
-    return filter_data
+    else:
+        return filter_data
