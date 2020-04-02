91cfeeb5ae0b424eb0e2a2aa355b046e13d163f9
Chitra07 <57441446+Chitra0790@users.noreply.github.com>
2020-04-02 12:25:29 +0100

    Added some comment

--- functions/get_lyrics.py
+++ functions/get_lyrics.py
@@ -18,24 +18,24 @@ def get_lyrics(df):
     filter_data = filter_data.reset_index(drop=True)
 
     for i in notebook.tqdm(range(len(filter_data.GENIUS_LINK))):
-        page = requests.get(filter_data.GENIUS_LINK[i])
+        page = requests.get(filter_data.GENIUS_LINK[i]) # Getting Html Tag for each links in Genius_link column 
         if page.status_code == 200:
             html = bs(page.text, 'html.parser')
             find_class = html.find('div', class_='lyrics')
             
             if find_class is not None:
-                lyrics = find_class.get_text()
+                lyrics = find_class.get_text() # getting text from the given class
                 lyrics = re.sub(r'[\(\[].*?[\)\]]', '', lyrics)
                 filter_data.loc[filter_data["GENIUS_LINK"] == filter_data["GENIUS_LINK"][i], "LYRICS"] = lyrics.lstrip()
             else:
-                find_class2 = html.find('div', class_='Lyrics__Container-sc-1ynbvzw-2 iVKelV')
+                find_class2 = html.find('div', class_='Lyrics__Container-sc-1ynbvzw-2 iVKelV') # Something lyrics are in different class
                 if find_class2 is not None:
                     lyrics = "\n".join(find_class2.strings)
                     lyrics = re.sub(r'[\(\[].*?[\)\]]', '', lyrics)
                     filter_data.loc[filter_data["GENIUS_LINK"] == filter_data["GENIUS_LINK"][i], "LYRICS"] = lyrics.lstrip()
                 
         else:
-            filter_data.loc[filter_data["GENIUS_LINK"] == filter_data.GENIUS_LINK[i], "LYRICS"] = None
-            time.sleep(2.5)    
+            filter_data.loc[filter_data["GENIUS_LINK"] == filter_data.GENIUS_LINK[i], "LYRICS"] = None # If Url not found
+            time.sleep(0.5)    
     
-    return (filter_data)
+    return filter_data
