264898f4388793e63497072f5faf97af867b2f10
Chitra07 <57441446+Chitra0790@users.noreply.github.com>
2020-04-30 16:41:21 +0100

    "Added Rating and Rating count columns"
    Used request for web-scraping...

--- functions/get_discogs.py
+++ functions/get_discogs.py
@@ -35,8 +35,8 @@ def get_track_genius(df):
     
     #Creating new columns -> removing any special char and replacing space by "-"
     df["new_artist"] = (df['ARTIST_NAME'].str.translate({ord(c): "" for c in "!@#$%^&*()[]{};:,./<>?\|`~=_+'"})).replace(" ", "-", regex = True)
-    df["new_albums"] = (df['ALBUMS'].str.translate({ord(c): "" for c in "!@#$%^&*()[]{};:,./<>?\|`~=_+'"})).replace(" ", "-", regex = True).replace("--", "-", regex = True)
-    ls_a_id, ls_type, ls_artist_name, ls_albums, ls_year, ls_album_type, ls_track, ls_genius_link = [], [], [], [], [], [], [], []
+    df["new_albums"] = ((df['ALBUMS'].str.replace(".", "-", regex = True)).str.translate({ord(c): "" for c in "!@#$%^&*()[]{};:,./<>?\|`~=_+'"})).replace(" ", "-", regex = True).replace("--", "-", regex = True)
+    ls_a_id, ls_type, ls_artist_name, ls_albums, ls_year, ls_album_type, ls_rating, ls_rating_count, ls_track, ls_genius_link = [], [], [], [], [], [], [], [], [], []
     artist_df = pd.DataFrame()
     print("Getting Track titles for each albums from Genius..")
     for i in notebook.tqdm(range(len(df))):
@@ -53,6 +53,8 @@ def get_track_genius(df):
                 ls_albums.append(df["ALBUMS"][i])
                 ls_year.append(df["YEAR"][i])
                 ls_album_type.append(df["ALBUMS_TYPES"][i])
+                ls_rating.append(df["RATING_OUT_OF_5"][i])
+                ls_rating_count.append(df["RATING_COUNT"][i])
                 ls_track.append((tags.get_text())) #appending track titles
                 ls_genius_link.append(links["href"]) # appending lyrics https links for each song title
         else:
@@ -62,6 +64,8 @@ def get_track_genius(df):
             ls_albums.append(df["ALBUMS"][i])
             ls_year.append(df["YEAR"][i])
             ls_album_type.append(df["ALBUMS_TYPES"][i])
+            ls_rating.append(df["RATING_OUT_OF_5"][i])
+            ls_rating_count.append(df["RATING_COUNT"][i])
             ls_track.append((None))
             ls_genius_link.append(None)
     
@@ -72,6 +76,8 @@ def get_track_genius(df):
     artist_df['ALBUMS'] = ls_albums
     artist_df['YEAR'] = ls_year
     artist_df["ALBUMS_TYPES"] = ls_album_type
+    artist_df["RATING_OUT_OF_5"] = ls_rating
+    artist_df["RATING_COUNT"] = ls_rating_count
     artist_df['TRACK_TITLE'] = ls_track
     artist_df['GENIUS_LINK'] = ls_genius_link
     #Cleaning Track title
@@ -88,7 +94,7 @@ def get_track_discog(df):
     @return: Dataframe containing track name and genuis http links
     '''
     reset_df = df.reset_index(drop = True)
-    ls_a_id, ls_type, ls_artist_name, ls_albums, ls_year, ls_album_type, ls_track = [], [], [], [], [], [], []
+    ls_a_id, ls_type, ls_artist_name, ls_albums, ls_year, ls_album_type, ls_rating, ls_rating_count, ls_track = [], [], [], [], [], [], [], [], []
     track_info = pd.DataFrame()
     print("Getting Track..")
     for i in notebook.tqdm(range(len(reset_df))):
@@ -104,6 +110,8 @@ def get_track_discog(df):
             ls_albums.append(reset_df["ALBUMS"][i])
             ls_year.append(reset_df["YEAR"][i])
             ls_album_type.append(reset_df["ALBUMS_TYPES"][i])
+            ls_rating.append(reset_df["RATING_OUT_OF_5"][i])
+            ls_rating_count.append(reset_df["RATING_COUNT"][i])
             ls_track.append(track.title)
 #             ls_genius_link.append("https://genius.com/{0}-{1}-lyrics".format())
     
@@ -114,6 +122,8 @@ def get_track_discog(df):
     track_info['ALBUMS'] = ls_albums
     track_info['YEAR'] = ls_year
     track_info["ALBUMS_TYPES"] = ls_album_type
+    track_info['TRACK_TITLE'] = ls_track
+    track_info["RATING_COUNT"] = ls_rating_count
     track_info['TRACK_TITLE'] = ls_track
     #Creating new columns to clean up any special char. 
     track_info["new_artist"] = (track_info['ARTIST_NAME'].str.translate({ord(c): "" for c in "!@#$%^&*()[]{};:,./<>?\|`~=_+'"})).replace(" ", "-", regex = True)
@@ -171,6 +181,53 @@ def getArtistID(artist_name):
     artist_nam = artist_data.split("'")[1]
     return (artist_id, artist_nam)
 
+
+def get_stat_link(url, df):
+    '''
+    Webscrapping artist page
+    Getting hyperlinks for the each album
+    
+    @param: url-> string [http link]
+    @param: df-> DataFrame
+    
+    @return: DataFrame [add new colum STAT_LINK]
+    '''
+    r_get = requests.get(url)
+    html = bs(r_get.text, 'html.parser')
+    find_class = html.findAll('td', class_= "title")
+    
+    print("Getting Albums links in Discog..")
+    # getting the links for the stat page
+    for i in notebook.tqdm(range(len(find_class))):
+        links = find_class[i].find("a")
+        album_ids = links["href"][links["href"].rfind("/")+1:]
+        df.loc[df["ID"] == album_ids, "STAT_LINK"] = "https://www.discogs.com" + links["href"]
+    
+    return df
+
+def get_album_stat(url, df):
+    '''
+    webscrapping album page
+    Getting rate value and rate count of the albums
+    
+    @param: url-> string [http link]
+    @param: df-> DataFrame
+    
+    @return: DataFrame [add new colum RATING_VALUE, RATING_COUNT]
+    '''
+    df_stat_link = get_stat_link(url, df)
+    # webscrapping each album website
+    for idx, row in df_stat_link.iterrows():
+        stat_request = requests.get(row["STAT_LINK"])
+        html_tags = bs(stat_request.text, 'html.parser')
+        rating = html_tags.find("span", class_="rating_value").get_text()
+        rating_count = html_tags.find("span", class_="rating_count").get_text()
+        df_stat_link.loc[df_stat_link["ID"] == row["ID"], "RATING_OUT_OF_5"] = rating
+        df_stat_link.loc[df_stat_link["ID"] == row["ID"], "RATING_COUNT"] = rating_count
+    
+    return df_stat_link.drop(['STAT_LINK'], axis=1)
+
+
 def extract(el, css_sel):
     ms = el.cssselect(css_sel)
     return None if len(ms) != 1 else ms[0].text
@@ -201,7 +258,7 @@ def get_artist_albums(a_name):
         
         if section_value == 0:
             album_id.append(row.get("data-object-id"))
-            artist_name.append(a_name)
+            artist_name.append(a_nam)
             types.append(row.get("data-object-type"))
             title.append(extract(row, ".title a"))
             formats.append(extract(row, ".title .format"))
@@ -214,7 +271,9 @@ def get_artist_albums(a_name):
     albums_info['ALBUMS_TYPES'] = formats
     albums_info['YEAR'] = year
     
-    return albums_info
+    update_albums_info = get_album_stat(url, albums_info)
+    
+    return update_albums_info
 
 def getArtistData(a_name):
     '''
@@ -242,4 +301,4 @@ def getArtistData(a_name):
         flag_data = flag_exclude_album(genius_track) # Creating flag column for an album
         final_data = flag_track_title(flag_data) # Creating flag column for a track_title
         
-    return final_data
+    return final_data      
