44a69d12aa4a4854b555f19f009de28b656454a2
Chitra07 <57441446+Chitra0790@users.noreply.github.com>
2020-07-09 22:22:54 +0100

    Added new RE to Clean Lyrics

--- functions/get_lyrics.py
+++ functions/get_lyrics.py
@@ -27,15 +27,23 @@ def getLyrics(df):
             
             if find_class is not None:
                 lyrics = find_class.get_text() # getting text from the given class
-                lyrics_clean1 = re.sub(r'\[.*?\]', '', lyrics)
-                filter_data.loc[filter_data["GENIUS_LINK"] == filter_data["GENIUS_LINK"][i], "LYRICS"] = lyrics_clean1
+                #Cleaning Lyrics
+                lyrics_remove1 = re.sub(r'\[.*?\]', '', lyrics)
+                lyrics_remove2 = [re.sub("[\\[].*?[\)\]]", "", char) for char in lyrics_remove1.split("\n")]
+                lyrics_remove3 = list(filter(None, lyrics_remove2))
+                lyrics_clean = '\n'.join(lyrics_remove3)
+                filter_data.loc[filter_data["GENIUS_LINK"] == filter_data["GENIUS_LINK"][i], "LYRICS"] = lyrics_clean
             else:
                 find_class2 = html.find('div', class_= regex1) # Sometime lyrics are in different class
                 if find_class2 is not None:
+                    #Cleaning Lyrics
                     lyrics_rm_1 = re.sub(r"(\w)([A-Z])", r'\1\n\2', find_class2.get_text()) # Insert line break between small and capital letter
                     lyrics_rm_2 = re.sub(r"(])([A-Z])", r'\1\n\2', lyrics_rm_1)# Insert line break between chart ] and capital letter
                     lyrics_rm_3 = re.sub(r"([?')])([A-Z])", r'\1\n\2', lyrics_rm_2) # Insert line break between chart ' or ? or ) and capital letter
-                    lyrics_clean2 = re.sub(r'\[.*?\]', '', lyrics_rm_3) # Remove any character within []
+                    lyrics_rm_4= re.sub(r'\[.*?\]', '', lyrics_rm_3) # Remove any character within []
+                    lyrics_rm_5 = [re.sub("[\\[].*?[\)\]]", "", char) for char in lyrics_rm_4.split("\n")]
+                    lyrics_rm_6 = list(filter(None, lyrics_rm_5))
+                    lyrics_clean2 = '\n'.join(lyrics_rm_6)
                     filter_data.loc[filter_data["GENIUS_LINK"] == filter_data["GENIUS_LINK"][i], "LYRICS"] = lyrics_clean2
                 
         else:
