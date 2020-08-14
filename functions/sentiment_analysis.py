39e413f9ad95fc596e36370a11083ee8086ed4f9
Chitra07 <57441446+Chitra0790@users.noreply.github.com>
2020-08-14 09:52:49 +0100

    Change notebook.tqdm to tqdm_notebook as per latest version. Added np.nan for sentiment columns with a condition.

--- functions/sentiment_analysis.py
+++ functions/sentiment_analysis.py
@@ -5,7 +5,7 @@ nltk.download('vader_lexicon')
 import numpy as np
 import pandas as pd
 
-from tqdm import tnrange, tqdm_notebook, notebook
+from tqdm import tnrange, tqdm_notebook
 
 # function
 
@@ -21,46 +21,53 @@ def sentimentAnalyser(df, artist):
         (df['ARTIST_NAME']==artist) & \
         (~df['LYRICS'].isna())
     ]['TRACK_TITLE']
-    for i in notebook.tqdm(range(len(song_list))):
-        for song in song_list:
-            # transform artist-song lyrics into a list
-            lyrics_list = df[(df['ARTIST_NAME'] == artist) & \
-                             (df['TRACK_TITLE'] == song)
-                            ]['LYRICS'].str.split('\n').tolist()[0]
-            num_negative=0
-            num_neutral =0
-            num_positive =0
-            compound = 0
-            for sentence in lyrics_list:
-                ss = sid.polarity_scores(sentence)
-                comp = ss['compound']
-                compound += comp
-                if comp >= 0.2:
-                    num_positive += 1
-                elif comp > -0.2 and comp < 0.2:
-                    num_neutral += 1
-                else:
-                    num_negative += 1
-            num_lines = num_negative+num_neutral+num_positive
-            df.loc[
-                (df['ARTIST_NAME'] == artist) & \
-                (df['TRACK_TITLE'] == song), \
-                "SENTIMENT_PCT_NEGATIVE"
-            ] = num_negative/num_lines*100
-            df.loc[
-                (df['ARTIST_NAME'] == artist) & \
-                (df['TRACK_TITLE'] == song), \
-                "SENTIMENT_PCT_NEUTRAL"
-            ] = num_neutral/num_lines*100
-            df.loc[
-                (df['ARTIST_NAME'] == artist) & \
-                (df['TRACK_TITLE'] == song), \
-                "SENTIMENT_PCT_POSITIVE"
-            ] = num_positive/num_lines*100
-            df.loc[
-                (df['ARTIST_NAME'] == artist) & \
-                (df['TRACK_TITLE'] == song), \
-                "SENTIMENT_COMPOUND_SCORE"
-            ] = compound/num_lines        
+    if len(song_list) == 0:
+        df["SENTIMENT_PCT_NEGATIVE"] = np.nan
+        df["SENTIMENT_PCT_NEUTRAL"] = np.nan
+        df["SENTIMENT_PCT_POSITIVE"] = np.nan
+        df["SENTIMENT_COMPOUND_SCORE"] = np.nan
+    
+    else:        
+        for i in tqdm_notebook(range(len(song_list))):
+            for song in song_list:
+                # transform artist-song lyrics into a list
+                lyrics_list = df[(df['ARTIST_NAME'] == artist) & \
+                                 (df['TRACK_TITLE'] == song)
+                                ]['LYRICS'].str.split('\n').tolist()[0]
+                num_negative=0
+                num_neutral =0
+                num_positive =0
+                compound = 0
+                for sentence in lyrics_list:
+                    ss = sid.polarity_scores(sentence)
+                    comp = ss['compound']
+                    compound += comp
+                    if comp >= 0.2:
+                        num_positive += 1
+                    elif comp > -0.2 and comp < 0.2:
+                        num_neutral += 1
+                    else:
+                        num_negative += 1
+                num_lines = num_negative+num_neutral+num_positive
+                df.loc[
+                    (df['ARTIST_NAME'] == artist) & \
+                    (df['TRACK_TITLE'] == song), \
+                    "SENTIMENT_PCT_NEGATIVE"
+                ] = num_negative/num_lines*100
+                df.loc[
+                    (df['ARTIST_NAME'] == artist) & \
+                    (df['TRACK_TITLE'] == song), \
+                    "SENTIMENT_PCT_NEUTRAL"
+                ] = num_neutral/num_lines*100
+                df.loc[
+                    (df['ARTIST_NAME'] == artist) & \
+                    (df['TRACK_TITLE'] == song), \
+                    "SENTIMENT_PCT_POSITIVE"
+                ] = num_positive/num_lines*100
+                df.loc[
+                    (df['ARTIST_NAME'] == artist) & \
+                    (df['TRACK_TITLE'] == song), \
+                    "SENTIMENT_COMPOUND_SCORE"
+                ] = compound/num_lines        
     
     return df
