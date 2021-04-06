dae479026744ad9ab0a2b9c1464daf1c175973cd
unknown <dusancernan@gmail.com>
2021-04-06 13:52:09 -0700

    #40 fix

--- functions/advanced_analytics.py
+++ functions/advanced_analytics.py
@@ -8,12 +8,25 @@ def plotDivergingBars(df, x, y, green, red, sort_by_values = True):
     else:
         data = df.sort_values(by=[y], ascending = False)
     data = data[[y,x]]
-    data['colors'] = [red if x < 0 else green for x in data[x]]
-    plt.figure(figsize=(14,max(1.5, len(data[y].unique())/2)),
+    plt.figure(figsize=(14,max(1, len(data[y].unique())/2)),
                dpi= 80)
-           
+    # artificial vertical padding
+    # make empty rows
+    first_row_dict = {}
+    first_row_dict[x] = [0]
+    first_row_dict[y] = [' ']
+    first_empty_row = pd.DataFrame.from_dict(first_row_dict)
+    last_row_dict = {}
+    last_row_dict[x] = [0]
+    last_row_dict[y] = ['']
+    last_empty_row = pd.DataFrame.from_dict(last_row_dict)
+    # add empty rows to the dataframe
+    data = first_empty_row.append(data.append(last_empty_row))
+    # add colours    
+    data['colors'] = [red if x < 0 else green for x in data[x]]
+    # plot lines
     plt.hlines(y=data[y], xmin=0, xmax=data[x], color=data.colors, alpha=0.6, linewidth=15)
-    plt.gca()
+    #plt.gca()
     plt.xlabel('Sentiment Score', fontsize=15)
     plt.ylabel('Track Title' if y == 'TRACK_TITLE' else 'Album', fontsize=15)
     plt.yticks(fontsize=12)
