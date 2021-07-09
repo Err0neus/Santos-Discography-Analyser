df6383b5709c9343674e4ca34c1cccb9a50dd87a
Chitra07 <57441446+Chitra0790@users.noreply.github.com>
2021-07-09 18:47:30 +0100

    Updated getLyrics -> added while loop
    While loop ->Fetching lyrics from each url if status_code  == 200.

--- functions/get_lyrics.py
+++ functions/get_lyrics.py
@@ -5,6 +5,40 @@ import time
 from bs4 import BeautifulSoup as bs
 from tqdm import tnrange, tqdm_notebook
 
+def fetch_lyrics(html_class):
+    '''
+    Getting Lyrics from html tag for each url
+    
+    @param: list of html tags
+    @return: string of lyrics with line break
+    '''
+    ls_lyrics = []
+    for web_tag in html_class:
+        lyrics = web_tag.get_text(separator='<br/>')# using <br/> tag as text seprater
+        ls_lyrics.append(lyrics)
+    # Cleaning Lyrics
+    concat_lyrics = ''.join(ls_lyrics)
+    line_break_lyrics = concat_lyrics.replace('<br/>', '\n')
+    lyrics_remove1 = re.sub(r'\[.*?\]', '', line_break_lyrics) # Removing any character within []
+    lyrics_remove2 = list(filter(None, lyrics_remove1.split('\n'))) # Removing any white space
+    lyrics_clean = "\n".join(lyrics_remove2) # adding line break with clean lyrics
+    
+    return lyrics_clean
+
+def get_list_of_tag(http_request):
+    '''
+    Get all the list of html tag with class-> Lyrics__Container
+    
+    return : list of html tags
+    '''
+    # Regular Expression that matches the class in HTML 
+    regex1 = re.compile('^Lyrics__Container.*')
+    
+    html = bs(http_request.text, 'html.parser')
+    find_class = html.find_all('div', class_= regex1)
+    
+    return find_class
+
 def getLyrics(df):
     '''
     Fetching lyric for each song using requests and Beautifulsoup libaries
@@ -16,39 +50,18 @@ def getLyrics(df):
     '''
     filter_data = df[(df["EXCLUDE_ALBUM"] == False) & (df["EXCLUDE_SONG"] == False)].copy(deep=True)
     filter_data = filter_data.reset_index(drop=True)
-    # Regular Expression that matches the class in HTML 
-    regex1 = re.compile('.*Lyrics*')
 
     for i in tqdm_notebook(range(len(filter_data.GENIUS_LINK))):
         page = requests.get(filter_data.GENIUS_LINK[i]) # Getting Html Tag for each links in Genius_link column 
         if page.status_code == 200:
-            html = bs(page.text, 'html.parser')
-            find_class = html.find('div', class_= 'lyrics')# Matching string for the class
-            
-            if find_class is not None:
-                lyrics = find_class.get_text() # getting text from the given class
-                #Cleaning Lyrics
-                lyrics_remove1 = re.sub(r'\[.*?\]', '', lyrics)
-                lyrics_remove2 = [re.sub("[\\[].*?[\)\]]", "", char) for char in lyrics_remove1.split("\n")]
-                lyrics_remove3 = list(filter(None, lyrics_remove2))
-                lyrics_clean = '\n'.join(lyrics_remove3)
-                filter_data.loc[filter_data["GENIUS_LINK"] == filter_data["GENIUS_LINK"][i], "LYRICS"] = lyrics_clean
-            else:
-                find_class2 = html.find('div', class_= regex1) # Sometime lyrics are in different class
-                if find_class2 is not None:
-                    #Cleaning Lyrics
-                    lyrics_rm_1 = re.sub(r"(\w)([A-Z])", r'\1\n\2', find_class2.get_text()) # Insert line break between small and capital letter
-                    lyrics_rm_2 = re.sub(r"(])([A-Z])", r'\1\n\2', lyrics_rm_1)# Insert line break between chart ] and capital letter
-                    lyrics_rm_3 = re.sub(r"([?')])([A-Z])", r'\1\n\2', lyrics_rm_2) # Insert line break between chart ' or ? or ) and capital letter
-                    lyrics_rm_4= re.sub(r'\[.*?\]', '', lyrics_rm_3) # Remove any character within []
-                    lyrics_rm_5 = [re.sub("[\\[].*?[\)\]]", "", char) for char in lyrics_rm_4.split("\n")]
-                    lyrics_rm_6 = [e for e in lyrics_rm_5 if e not in ('Embed', 'Share Url:Copy', 'Embed:Copy', '')] # removing
-#                     lyrics_rm_6 = list(filter(None, lyrics_rm_5))
-                    lyrics_clean2 = '\n'.join(lyrics_rm_6)
-                    filter_data.loc[filter_data["GENIUS_LINK"] == filter_data["GENIUS_LINK"][i], "LYRICS"] = lyrics_clean2
-                
+            find_class = []
+            while find_class == []: # Refreshing page if the find_class is empty but status_code = 200
+                page = requests.get(filter_data.GENIUS_LINK[i])
+                find_class = get_list_of_tag(page)
+            lyrics = fetch_lyrics(find_class)
+            filter_data.loc[filter_data["GENIUS_LINK"] == filter_data["GENIUS_LINK"][i], "LYRICS"] = lyrics
+
         else:
-            filter_data.loc[filter_data["GENIUS_LINK"] == filter_data.GENIUS_LINK[i], "LYRICS"] = None # If Url not found
-            time.sleep(0.5)    
+            filter_data.loc[filter_data["GENIUS_LINK"] == filter_data.GENIUS_LINK[i], "LYRICS"] = None # If Url not found   
     
     return filter_data
